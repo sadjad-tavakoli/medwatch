6f17dad38dec6d1c9b19aa451c5909120aff6057
Codelopers Admin <codelopers@gmail.com>
2016-11-22 00:12:16 +0330

    in

---
+++ med_watch/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for med_watch project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "med_watch.settings")
+
+application = get_wsgi_application()
