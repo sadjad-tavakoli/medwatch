6561667a658533ccec427ea31756850f0407ae0e
Codelopers Admin <codelopers@gmail.com>
2017-01-19 16:35:22 +0330

    push

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,3 +1,9 @@
+
 from django.contrib import admin
 
 # Register your models here.
+from schedule.models import DoctorSchedule, DailySchedule, AppointmentRequest
+
+admin.site.register(DoctorSchedule)
+admin.site.register(DailySchedule)
+admin.site.register(AppointmentRequest)
