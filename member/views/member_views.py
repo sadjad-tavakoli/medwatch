e2e2db8688a380977fb8eace60b1db1d217053cd
Zahra <zkhanalizade@gmail.com>
2017-02-04 13:04:59 +0330

    search in doctors

---
+++ member/views/member_views.py
@@ -0,0 +1,21 @@
+from django.views.generic.base import View
+from django.db.models import Q
+from django.shortcuts import render
+
+# from member
+from member import models
+
+def SearchInDoctors(request):
+
+    name = request.POST.get('name_search', "")
+    str = name.split(" ")
+    result_doctors = []
+    for item in str:
+        if models.DoctorMember.objects.filter(Q(first_name=item) | Q(last_name=item)):
+            print("appendid")
+            result_doctors.append(models.DoctorMember.objects.filter(Q(first_name=item) | Q(last_name=item)))
+
+    print(result_doctors)
+
+
+    return render(request, 'member/search_in_doctors.html', {'result_doctors': result_doctors})
